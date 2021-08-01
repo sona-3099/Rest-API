f899b5b7b09d1651147b3d092b01b0dd334cbd26
Wavare Sonali <62466919+sona-3099@users.noreply.github.com>
2021-08-01 11:44:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ScientistInformation from './components/ScientistInformation';
+
+function App() {
+  return (
+    <ScientistInformation />
+  );
+}
+
+export default App;
